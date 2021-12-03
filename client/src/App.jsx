a3218c4a95832b06e836c1edfda86e839ec33461
shahazad08 <sk.shahazad@gmail.com>
2021-12-03 19:23:59 +0530

    Added: Front End File depedencies added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	return (
+		<Router>
+			<Routes path="/" exact component={Join} />
+			<Routes path="/chat" component={Chat} />
+		</Router>
+	);
+};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
-
-export default App
+export default App;
